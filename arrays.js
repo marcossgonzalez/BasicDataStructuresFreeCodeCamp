2e633effe239413ed9c2c091920c1bfd6c70042f
Marcos Silveira Gonzalez Júnior <marcos.s.gonzalezjr@outlook.com>
2023-10-05 14:48:31 -0700

    Update lesson
    update lesson

--- arrays.js
+++ arrays.js
@@ -53,3 +53,25 @@ function forecast(arr) {
   
   // Only change code above this line
   console.log(forecast(['cold', 'rainy', 'warm', 'sunny', 'cool', 'thunderstorms']));
+//------------------------------------
+  function copyMachine(arr, num) {
+    let newArr = [];
+    while (num >= 1) {
+      // Only change code below this line
+      newArr.push([...arr]);
+      // Only change code above this line
+      num--;
+    }
+    return newArr;
+  }
+  
+  console.log(copyMachine([true, false, true], 2));
+//------------------------------------
+function spreadOut() {
+    let fragment = ['to', 'code'];
+    let sentence = ["learning",...fragment,"is", "fun"]; // Change this line
+    return sentence;
+  }
+  
+  console.log(spreadOut());
+//------------------------------------
