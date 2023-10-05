5192157af8ca32ed3e366353929ed8414dd855e1
Marcos Silveira Gonzalez Júnior <marcos.s.gonzalezjr@outlook.com>
2023-10-05 14:57:24 -0700

    Update lesson
    update lesson

--- arrays.js
+++ arrays.js
@@ -75,3 +75,16 @@ function spreadOut() {
   
   console.log(spreadOut());
 //------------------------------------
+function quickCheck(arr, elem) {
+    // Only change code below this line
+    if(arr.indexOf(elem) == -1){
+      return false;
+    }
+    else if (arr.indexOf(elem) >= 0){
+      return true;
+    }
+    // Only change code above this line
+  }
+  
+  console.log(quickCheck(['squash', 'onions', 'shallots'], 'mushrooms'));
+//------------------------------------
