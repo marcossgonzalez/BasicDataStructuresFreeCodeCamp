8ae9c633a5f042aec1c4f185ccfa2afa0df69f7d
Marcos Silveira Gonzalez Júnior <marcos.s.gonzalezjr@outlook.com>
2023-10-08 23:09:35 -0700

    Update lesson
    update lesson

--- objects.js
+++ objects.js
@@ -49,5 +49,76 @@ let foods = {
 /*Se quisermos remover a chave apples, podemos removê-la usando a palavra-chave delete como esta:
 delete foods.apples;*/
 //-------------------------------------------
+let foods = {
+    apples: 25,
+    oranges: 32,
+    plums: 28,
+    bananas: 13,
+    grapes: 35,
+    strawberries: 27
+  };
+  
+  // Only change code below this line
+  delete foods.oranges;
+  delete foods.plums;
+  delete foods.strawberries;
+  
+  // Only change code above this line
+  
+  console.log(foods);
+//-------------------------------------------
+/*Agora podemos adicionar, modificar e remover chaves de objetos. Mas e se quiséssemos apenas saber se um objeto possui uma propriedade 
+específica? JavaScript nos fornece duas maneiras diferentes de fazer isso. Um usa o método hasOwnProperty() e o outro usa a 
+palavra-chave in. Se tivermos um objeto users com uma propriedade Alan, poderemos verificar sua presença de uma das seguintes maneiras:
 
+users.hasOwnProperty('Alan');
+'Alan' in users;*/
+//-------------------------------------------
+let users = {
+    Alan: {
+      age: 27,
+      online: true
+    },
+    Jeff: {
+      age: 32,
+      online: true
+    },
+    Sarah: {
+      age: 48,
+      online: true
+    },
+    Ryan: {
+      age: 19,
+      online: true
+    }
+  };
+  
+  function isEveryoneHere(userObj) {
+    // Only change code below this line
+    let array = ["Alan", "Jeff", "Sarah", "Ryan"];
+    for (let i = 0; i<4;i++ ){
+      return userObj.hasOwnProperty(array[i]) &&
+      userObj.hasOwnProperty(array[i+1]) &&
+      userObj.hasOwnProperty(array[i+2]) &&
+      userObj.hasOwnProperty(array[i+3]);
+      }
+  
+      
+    // Only change code above this line
+  }
+  
+  console.log(isEveryoneHere(users));
+//-------------------------------------------
+/*Às vezes você precisa iterar todas as chaves dentro de um objeto. 
+Você pode usar um loop for...in para fazer isso. O loop for...in se parece com:
+
+const refrigerator = {
+  'milk': 1,
+  'eggs': 12,
+};
+
+for (const food in refrigerator) {
+  console.log(food, refrigerator[food]);
+}
+*/
 //-------------------------------------------
